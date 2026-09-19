3cb60c904559e5a0dec63955db9f3ae4a400c1fb
Faysal <ewrfaysu@gmail.com>
2026-09-19 11:11:42 +0600

    Change API key validation from 'luffy' to 'zoro'

--- app.py
+++ app.py
@@ -310,7 +310,7 @@ def handle_requests():
     key = request.args.get("key")
     client_ip = request.remote_addr
 
-    if key != "luffy":
+    if key != "zoro":
         return jsonify({"error": "Invalid or missing API key 🔑"}), 403
 
     if not uid or not server_name:
